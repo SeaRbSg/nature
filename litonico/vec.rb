80e5c09044d8172b77f6e5de96bd32af97e8a61f
Lito Nicolai <lito.nicolai@gmail.com>
2016-02-05 22:59:02 -0800

    N-dimensional vectors

--- litonico/vec.rb
+++ litonico/vec.rb
@@ -1,28 +1,31 @@
-class Vec2
-  attr_accessor :x, :y
-  def initialize x, y
-    @x = x
-    @y = y
-  end
+class Vec
+  attr_reader :contents
 
   def + other
-    Vec2.new x + other.x, y + other.y
+    vec = self.contents.zip(other.contents).map { |s, o| s + o }
+    self.class.new(*vec)
   end
 
   def - other
-    Vec2.new x - other.x, y - other.y
+    vec = self.contents.zip(other.contents).map { |s, o| s - o }
+    self.class.new(*vec)
+  end
+
+  def dot other
+    self.contents.zip(other).map { |s, o| s*o }.reduce { |sum, elem| sum + elem }
   end
 
   def scale s
-    Vec2.new x*s,  y*s
+    vec = self.contents.map { |elem| elem*s }
+    self.class.new(*vec)
   end
 
   def == other
-    x == other.x && y == other.y
+    self.zip(other).all {|s, o| s == o}
   end
 
   def magnitude_squared
-    x**2 + y**2
+    self.contents.map {|elem| elem**2 }.reduce { |sum, elem| sum + elem }
   end
 
   def magnitude
@@ -32,9 +35,10 @@ class Vec2
   def normalize
     m = self.magnitude
     if m == 0
-      Vec2.new 0, 0
+      self.zero
     else
-      Vec2.new x.to_f/m, y.to_f/m
+      vec = self.contents.map { |elem| elem.to_f/m }
+      self.class.new(*vec)
     end
   end
 
@@ -49,7 +53,7 @@ class Vec2
   def clamp s
     m = self.magnitude
     if m == 0.0
-      Vec2.new 0, 0
+      self.zero
     elsif m > s
       self.scale s.to_f/m
     else
@@ -57,18 +61,71 @@ class Vec2
     end
   end
 
+  def self.zero
+    vec = Array.new(self.contents.length, 0)
+    self.class.new vec
+  end
+end
+
+class Vec2 < Vec
+  attr_reader :x, :y
+  def initialize x, y
+    @contents = [x, y]
+  end
+
+  def x
+    self.contents[0]
+  end
+
+  def y
+    self.contents[1]
+  end
+
   def self.random
     angle = rand * 2.0*Math::PI
-    Vec2.new Math.cos(angle), Math.sin(angle)
+    self.class.new Math.cos(angle), Math.sin(angle)
   end
 
   def self.random_positive
-    Vec2.new rand, rand
+    self.zero.contents.map { |elem| rand }
   end
 
   def map &block
     Vec2.new block.call(x), block.call(y)
   end
 end
+
+class Vec3 < Vec
+  attr_reader :x, :y, :z
+  def initialize x, y, z
+    @contents = [x, y, z]
+  end
+
+  def x
+    self.contents[0]
+  end
+
+  def y
+    self.contents[1]
+  end
+
+  def z
+    self.contents[2]
+  end
+
+  def self.random
+    angle = rand * 2.0*Math::PI
+    self.class.new Math.cos(angle), Math.sin(angle)
+  end
+
+  def self.random_positive
+    self.zero.contents.map { |elem| rand }
+  end
+
+  def map &block
+    Vec2.new block.call(x), block.call(y)
+  end
+end
+
 
 ORIGIN = Vec2.new 0.0, 0.0
