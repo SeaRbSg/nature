c444cf1f3de93a82802f4195925e88a98336842f
Lito Nicolai <lito.nicolai@gmail.com>
2016-04-02 11:54:12 -0700

    vector contents -> components

--- litonico/vec.rb
+++ litonico/vec.rb
@@ -1,22 +1,22 @@
 class Vec
-  attr_reader :contents
+  attr_reader :components
 
   def + other
-    vec = self.contents.zip(other.contents).map { |s, o| s + o }
+    vec = self.components.zip(other.components).map { |s, o| s + o }
     self.class.new(*vec)
   end
 
   def - other
-    vec = self.contents.zip(other.contents).map { |s, o| s - o }
+    vec = self.components.zip(other.components).map { |s, o| s - o }
     self.class.new(*vec)
   end
 
   def dot other
-    self.contents.zip(other).map { |s, o| s*o }.reduce { |sum, elem| sum + elem }
+    self.components.zip(other).map { |s, o| s*o }.reduce(:+)
   end
 
   def scale s
-    vec = self.contents.map { |elem| elem*s }
+    vec = self.components.map { |elem| elem*s }
     self.class.new(*vec)
   end
 
@@ -25,7 +25,7 @@ class Vec
   end
 
   def magnitude_squared
-    self.contents.map {|elem| elem**2 }.reduce { |sum, elem| sum + elem }
+    self.components.map {|elem| elem**2 }.reduce(:+)
   end
 
   def magnitude
@@ -37,7 +37,7 @@ class Vec
     if m == 0
       self.zero
     else
-      vec = self.contents.map { |elem| elem.to_f/m }
+      vec = self.components.map { |elem| elem.to_f/m }
       self.class.new(*vec)
     end
   end
@@ -62,7 +62,7 @@ class Vec
   end
 
   def self.zero
-    vec = Array.new(self.contents.length, 0)
+    vec = Array.new(self.components.length, 0)
     self.class.new vec
   end
 end
@@ -70,15 +70,15 @@ end
 class Vec2 < Vec
   attr_reader :x, :y
   def initialize x, y
-    @contents = [x, y]
+    @components = [x, y]
   end
 
   def x
-    self.contents[0]
+    self.components[0]
   end
 
   def y
-    self.contents[1]
+    self.components[1]
   end
 
   def self.random
@@ -87,7 +87,7 @@ class Vec2 < Vec
   end
 
   def self.random_positive
-    self.zero.contents.map { |elem| rand }
+    self.zero.components.map { |elem| rand }
   end
 
   def map &block
@@ -98,19 +98,19 @@ end
 class Vec3 < Vec
   attr_reader :x, :y, :z
   def initialize x, y, z
-    @contents = [x, y, z]
+    @components = [x, y, z]
   end
 
   def x
-    self.contents[0]
+    self.components[0]
   end
 
   def y
-    self.contents[1]
+    self.components[1]
   end
 
   def z
-    self.contents[2]
+    self.components[2]
   end
 
   def self.random
@@ -119,13 +119,14 @@ class Vec3 < Vec
   end
 
   def self.random_positive
-    self.zero.contents.map { |elem| rand }
+    self.zero.components.map { |elem| rand }
   end
 
   def map &block
-    Vec2.new block.call(x), block.call(y)
+    Vec2.new block.call(x), block.call(y), block.call(z)
   end
 end
 
 
 ORIGIN = Vec2.new 0.0, 0.0
+V_ZERO = Vec2.new 0.0, 0.0
